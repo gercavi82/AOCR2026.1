15fe1cb3c45990712f939fdba6f08eef291e55e1
gercavi82 <147272385+gercavi82@users.noreply.github.com>
2026-03-04 13:58:52 -0500

    facturacion implementacion

---
+++ .history/CapaPresentacion/Controllers/ChecklistController_20260304135848.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using CapaModelo;
+
+namespace CapaPresentacion.Controllers
+{
+    [Authorize]
+    public class ChecklistController : Controller
+    {
+        // GET: Checklist
+        public ActionResult Index()
+        {
+            try
+            {
+                // TODO: Implement listing via ChecklistDAO when ObtenerTodos is available
+                return View(new List<Checklist>());
+            }
+            catch (Exception ex)
+            {
+                TempData["Error"] = "Error al cargar checklists: " + ex.Message;
+                return View(new List<Checklist>());
+            }
+        }
+
+        // GET: Checklist/Crear
+        public ActionResult Crear()
+        {
+            return View(new Checklist());
+        }
+
+        // POST: Checklist/Crear
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Crear(Checklist checklist)
+        {
+            try
+            {
+                // TODO: Wire to ChecklistBL.Insertar when Checklist-level CRUD is available
+                TempData["Success"] = "Checklist creado correctamente.";
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                TempData["Error"] = "Error al crear checklist: " + ex.Message;
+                return View(checklist);
+            }
+        }
+
+        // GET: Checklist/Editar/5
+        public ActionResult Editar(int id)
+        {
+            try
+            {
+                // TODO: Implement ObtenerPorId
+                return View(new Checklist { CodigoChecklist = id });
+            }
+            catch (Exception ex)
+            {
+                TempData["Error"] = "Error al cargar checklist: " + ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
+
+        // POST: Checklist/Editar
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Editar(Checklist checklist)
+        {
+            try
+            {
+                TempData["Success"] = "Checklist actualizado correctamente.";
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                TempData["Error"] = "Error al actualizar checklist: " + ex.Message;
+                return View(checklist);
+            }
+        }
+
+        // GET: Checklist/Detalle/5
+        public ActionResult Detalle(int id)
+        {
+            try
+            {
+                // TODO: Load real data
+                return View(new Checklist { CodigoChecklist = id });
+            }
+            catch (Exception ex)
+            {
+                TempData["Error"] = "Error al cargar checklist: " + ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
---
+++ .history/CapaPresentacion/Controllers/LogController_20260304135848.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace CapaPresentacion.Controllers
+{
+    [Authorize(Roles = "Administrador")]
+    public class LogController : Controller
+    {
+        // GET: Log
+        public ActionResult Index()
+        {
+            return View(new List<object>());
+        }
+    }
+}
